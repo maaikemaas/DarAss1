705b672fa45849e62415d3f98970632ace7a0154
maaikemaas <maaikemaas@msn.com>
2016-05-11 16:01:51 +0200

    create connection

--- DarAss1/DarAss1/Program.cs
+++ DarAss1/DarAss1/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
 
 namespace DarAss1
 {
@@ -10,6 +12,16 @@ namespace DarAss1
     {
         static void Main(string[] args)
         {
+            //make connection to database
+            SQLiteConnection m_dbConnection;
+            m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            m_dbConnection.Open();
+
+            //input file with sql things
+            string input = new StreamReader("autotable.txt").ReadToEnd();
+
+            //create table in sql with input text
+            SQLiteCommand command = new SQLiteCommand(input, m_dbConnection);
         }
     }
 }
