d95fde59a2da5ccff61950c878feb0aac0abca1c
maaikemaas <maaikemaas@msn.com>
2016-05-18 09:12:34 +0200

    werk aan idf methode

--- DarAss1/DarAss1/Program.cs
+++ DarAss1/DarAss1/Program.cs
@@ -8,7 +8,7 @@ using System.IO;
 
 namespace DarAss1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -24,6 +24,40 @@ namespace DarAss1
             SQLiteCommand command = new SQLiteCommand(input, m_dbConnection);
             command.ExecuteNonQuery();
             Console.ReadKey();
+        }
+
+        public void IDFsim(SQLiteConnection db, string query)
+        {
+            string[] querystring = query.Split(" ");
+            List<string> querylist = new List<string>();
+            
+
+            SQLiteCommand command = new SQLiteCommand("select ")
+            //if attribute = categoric
+            double idf = IDFcat();
+
+            //SqlConnection sqlConnection1 = new SqlConnection("Your Connection String");
+            //SqlCommand cmd = new SqlCommand();
+            //SqlDataReader reader;
+
+            //cmd.CommandText = "SELECT * FROM Customers";
+            //cmd.CommandType = CommandType.Text;
+            //cmd.Connection = sqlConnection1;
+
+            //sqlConnection1.Open();
+
+            //reader = cmd.ExecuteReader();
+            //// Data is accessible through the DataReader object here.
+
+            //sqlConnection1.Close();
+
+            //if attribute = numeric, take another
+            //or just make new table/column in database with categories
+        }
+
+        public double IDFcat()
+        {
+            return 1.0;
         }
     }
 }
