08063d552c6db07deed5e9d1df910d855122264c
maaikemaas <maaikemaas@msn.com>
2016-05-18 16:01:21 +0200

    fix verkeerde commit

--- DarAss1/DarAss1/Program.cs
+++ DarAss1/DarAss1/Program.cs
@@ -8,7 +8,7 @@ using System.IO;
 
 namespace DarAss1
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
@@ -27,42 +27,8 @@ namespace DarAss1
 
             string test_query = "k = 6, brand = 'volkswagen', cylinders = '6', mpg = '45';";
             Preprocessor p = new Preprocessor(test_query);
-            
+
             Console.ReadKey();
-        }
-
-        public void IDFsim(SQLiteConnection db, string query)
-        {
-            string[] querystring = query.Split(" ");
-            List<string> querylist = new List<string>();
-            
-
-            SQLiteCommand command = new SQLiteCommand("select ")
-            //if attribute = categoric
-            double idf = IDFcat();
-
-            //SqlConnection sqlConnection1 = new SqlConnection("Your Connection String");
-            //SqlCommand cmd = new SqlCommand();
-            //SqlDataReader reader;
-
-            //cmd.CommandText = "SELECT * FROM Customers";
-            //cmd.CommandType = CommandType.Text;
-            //cmd.Connection = sqlConnection1;
-
-            //sqlConnection1.Open();
-
-            //reader = cmd.ExecuteReader();
-            //// Data is accessible through the DataReader object here.
-
-            //sqlConnection1.Close();
-
-            //if attribute = numeric, take another
-            //or just make new table/column in database with categories
-        }
-
-        public double IDFcat()
-        {
-            return 1.0;
         }
     }
 }
